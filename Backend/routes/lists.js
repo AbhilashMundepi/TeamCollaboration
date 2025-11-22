63d931b73530bccf7bdf3c6cf03f323280c7f1fa
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2025-11-22 14:03:57 +0530

    Remove commented routes from lists.js

--- Backend/routes/lists.js
+++ Backend/routes/lists.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const taskCtrl = require('../controllers/taskController');
 const listCtrl = require('../controllers/listController');
 
-// GET /api/lists/:id/tasks
+
 router.get('/:id/tasks', taskCtrl.getTasksByList);
 
-// POST /api/lists/:id/tasks
+
 router.post('/:id/tasks', taskCtrl.createTask);
 
-// DELETE /api/lists/:id
+
 router.delete('/:id', listCtrl.deleteList);
 
 module.exports = router;
