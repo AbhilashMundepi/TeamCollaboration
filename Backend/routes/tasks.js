9cfd6133bd9a6cdb0511320d158d9e2656a05e4c
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2025-11-22 14:04:20 +0530

    Clean up task routes by removing comments
    Removed commented-out PUT and DELETE routes for tasks.

--- Backend/routes/tasks.js
+++ Backend/routes/tasks.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const taskCtrl = require('../controllers/taskController');
 
-// PUT /api/tasks/:id
+
 router.put('/:id', taskCtrl.updateTask);
 
-// DELETE /api/tasks/:id
 router.delete('/:id', taskCtrl.deleteTask);
 
 module.exports = router;
